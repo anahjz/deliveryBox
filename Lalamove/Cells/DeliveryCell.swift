58833245ddfeafda3ef4c425ae241fe8dae17ca6
AnaHjz <ana.hedjazi68@gmail.com>
2019-11-19 17:27:20 +0330

    Set delivery datas to delivery cell.

--- Lalamove/Cells/DeliveryCell.swift
+++ Lalamove/Cells/DeliveryCell.swift
@@ -7,3 +7,58 @@
 //
 
 import Foundation
+import UIKit
+import SDWebImage
+
+class DeliveryCell: UITableViewCell {
+    
+    @IBOutlet weak var deliveryImage: UIImageView!
+    @IBOutlet weak var deliveryAddress: UILabel!
+    @IBOutlet weak var deliveryDescription: UILabel!
+    
+    var delivery: DeliveryData? {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    
+    private func updateUI(){
+        
+        func updateDeliveryImage(){
+            
+            let imageUrl = URL(string: delivery?.imageUrl ?? "https://assets-global.website-files.com/5ab9f6adcd72e6536e58c8d2/5bff60461acf7f414f42f43e_lalamove-hero.jpg")
+            self.deliveryImage.sd_setImage(with: imageUrl, placeholderImage: UIImage(named: "placeHolder"))
+            
+        }
+        
+        func updateDeliveryAddress() {
+            
+            guard let delAddress = delivery?.address else {
+                
+                return self.deliveryAddress.text = "Unknown address!!"
+                
+            }
+            
+            self.deliveryAddress.text = delAddress
+            
+        }
+        
+        func updateDeliveryDescription() {
+            
+            guard let delDescription = delivery?.descriptions else {
+                
+                return self.deliveryDescription.text = "No description!!"
+                
+            }
+            
+            self.deliveryDescription.text = delDescription
+            
+        }
+        
+        updateDeliveryAddress()
+        updateDeliveryDescription()
+        updateDeliveryImage()
+    
+    }
+}
