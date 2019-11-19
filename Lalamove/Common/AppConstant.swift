42748d636d08dea37c59607a356cb19cc3e12bd8
AnaHjz <ana.hedjazi68@gmail.com>
2019-11-19 17:26:39 +0330

    Added base url in app constant.

---
+++ Lalamove/Common/AppConstant.swift
@@ -0,0 +1,13 @@
+//
+//  AppConstant.swift
+//  Lalamove
+//
+//  Created by admin on 11/18/19.
+//  Copyright © 2019 AnaHejazi. All rights reserved.
+//
+
+import Foundation
+struct AppConstants {
+    
+    static let baseUrl:  String = "https://mock-api-mobile.dev.lalamove.com"
+}
